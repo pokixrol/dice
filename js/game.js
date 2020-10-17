f3ac8726463752bb833c245c90dcaac5cb1aa920
unknown <kaja.ricna@seznam.cz>
2020-10-17 20:00:32 +0200

    Javascript transcript + another cube

--- js/game.js
+++ js/game.js
@@ -1,54 +1,113 @@
-var hody = [];
+const kostka = document.getElementById('kostka');
+const statistika = document.getElementById('statistika');
+const tlacitko = document.getElementById('tlacitko');
+let hod = 1;
+let hody = [];
+let timer = false;
 
-document.getElementById('game').addEventListener('click',
-    function(){
-        hod();
-        console.log(hody);
+function animace () {
+    hod = Math.ceil (Math.random() * 6);
+    kostka.src = 'img/kostka' + hod + '.png';
+}
+tlacitko.addEventListener('click', function(){
+    if(!timer){
+        timer = setInterval(animace, 50);
+        tlacitko.innerText = 'Stop';
+    }else{
+        clearInterval(timer);
+        timer = false;
+        tlacitko.innerText = 'Play';
+        hody.push(hod);
+        vypisStatistiky();
     }
-);
+    
+})
 
-function suma(cisla) {
-    var sum = 0;
-    cisla.forEach(function(value,index){
-        sum += value;
-    })
+function suma(){
+    let sum = 0;
+    for (let i = 0; i < hody.length; i++) {
+        sum += hody[i];
+    }
     return sum;
 }
 
-function maximum(cisla) {
-    var max = 1;
-    cisla.forEach(function(value,index){
-        if (value > max) max = value;
+function max(){
+    let maximum = 1;
+    hody.forEach(function(value, index){
+        if (value > maximum) maximum = value;
     })
-    return max;
+    return maximum;
 }
 
-function minimum(cisla) {
-    var min = 6;
-    cisla.forEach(function(value,index){
-        if (value < min) min = value;
+function min(){
+    let minimum = 6;
+    hody.forEach(function(value, index){
+        if (value < minimum) minimum = value;
     })
-    return min;
+    return minimum;
+}
+function vypisStatistiky (){
+    statistika.innerHTML = `<p>Poslední hod: ${hod}</p>`;
+    statistika.innerHTML += `<p>Počet hodů: ${hody.length}</p>`;
+    statistika.innerHTML += `<p>Součet: ${suma()}</p>`;
+    statistika.innerHTML += `<p>Průměr: ${(suma() / hody.length).toFixed(2)}</p>`;
+    statistika.innerHTML += `<p>Maximum: ${max()}</p>`;
+    statistika.innerHTML += `<p>Minimum: ${min()}</p>`;
 }
 
-function average(sum, count) {
-    return (sum / count).toFixed(2);
+const dvanactkostka = document.getElementById('dvanactkostka');
+const dvanactstatistika = document.getElementById('dvanactstatistika');
+const dvanacttlacitko = document.getElementById('dvanacttlacitko');
+let hod12 = 1;
+let hody12 = [];
+let timer12 = false;
+
+function animace12 () {
+    hod12 = Math.ceil (Math.random() * 12);
+dvanactkostka.src = '12kostka/12kostka' + hod12 + '.png';
+}
+dvanacttlacitko.addEventListener('click', function(){
+    if(!timer12){
+        timer12 = setInterval(animace12, 50);
+        dvanacttlacitko.innerText = 'Stop';
+    }else{
+        clearInterval(timer12);
+        timer12 = false;
+        dvanacttlacitko.innerText = 'Play';
+        hody12.push(hod12);
+        vypisStatistiky12();
+    }
+    
+})
+
+function suma12(){
+    let sum = 0;
+    for (let i = 0; i < hody.length; i++) {
+        sum += hody[i];
+    }
+    return sum;
 }
 
-function hod() {
-    var h = Math.ceil(Math.random() * 6);
-    hody.push(h);
-    document.getElementById('cube').src='img/kostka' + h + '.png';
-    document.getElementById('result').innerHTML = '<p>Hod: ' + h + '</p>';
-    document.getElementById('result').innerHTML += 
-        '<p>Počet hodů: ' + hody.length + '</p>';
-    document.getElementById('result').innerHTML += 
-        '<p>Součet hodů: ' + suma(hody) + '</p>';
-    document.getElementById('result').innerHTML += 
-        '<p>Průměr hodů: ' + average(suma(hody),hody.length) + '</p>';
-    document.getElementById('result').innerHTML += 
-        '<p>Nejvyšší hod: ' + maximum(hody) + '</p>';
-    document.getElementById('result').innerHTML += 
-        '<p>Nejvyšší hod: ' + minimum(hody) + '</p>';
-    return h;
+function max12(){
+    let maximum = 1;
+    hody12.forEach(function(value, index){
+        if (value > maximum) maximum = value;
+    })
+    return maximum;
+}
+
+function min12(){
+    let minimum = 6;
+    hody12.forEach(function(value, index){
+        if (value < minimum) minimum = value;
+    })
+    return minimum;
+}
+function vypisStatistiky12 (){
+    dvanactstatistika.innerHTML = `<p>Poslední hod: ${hod12}</p>`;
+    dvanactstatistika.innerHTML += `<p>Počet hodů: ${hody12.length}</p>`;
+    dvanactstatistika.innerHTML += `<p>Součet: ${suma12()}</p>`;
+    dvanactstatistika.innerHTML += `<p>Průměr: ${(suma12() / hody12.length).toFixed(2)}</p>`;
+    dvanactstatistika.innerHTML += `<p>Maximum: ${max12()}</p>`;
+    dvanactstatistika.innerHTML += `<p>Minimum: ${min12()}</p>`;
 }
